ea7d476023d5b9fa311941afb4151b2d0ede0e6b
Sizhe Wei <szweisjtu@gmail.com>
2025-08-24 18:10:58 -0400

    Connect all foot nodes.

--- go2_gym_learn/ppo_cse/my_gnn.py
+++ go2_gym_learn/ppo_cse/my_gnn.py
@@ -146,6 +146,11 @@ class myGNN(torch.nn.Module):
         # Define all connections in a single edge_index tensor        
         node_2_node = torch.tensor([[0, 0, 0, 0, 1, 2, 4, 5, 7, 8, 10, 11],
                                     [1, 4, 7, 10, 2, 3, 5, 6, 8, 9, 11, 12]])
+        
+        # TODO: Connect all foot nodes, TESTING!
+        node_2_node = torch.cat([node_2_node,
+                                 torch.tensor([[3, 9, 3, 6, 3, 6],
+                                               [6, 12, 9, 12, 12, 9]])], dim=1)
 
         edge_index = torch.cat([
             node_2_node, node_2_node.flip(0)
